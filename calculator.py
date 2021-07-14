d8795b3a4d84300f364be1d4dcc384a166370bb6
sierra hughey <sierrahughey@mailbox.org>
2021-07-14 22:59:12 +0000

    added equations to arthmetic, added options to calculator

--- calculator.py
+++ calculator.py
@@ -5,3 +5,36 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    input_string = input()
+    token = input_string.split(' ')
+    if token[0] == 'q':
+        break
+    else:
+        if token[0] == '+':
+            result = (add(float(token[1]), float(token[2])))
+        
+        elif token [0] == '-':
+            result = (subtract(float(token[1]), float(token[2])))
+            
+        elif token [0] == '*':
+            result = (multiply(float(token[1]), float(token[2])))
+
+        elif token [0] == '/':
+            result = (divide(float(token[1]), float(token[2])))
+
+        elif token [0] == 'square':
+            result = (square(float(token[1])))
+            
+        elif token[0] == 'cube':
+            result = (cube(float(token[1])))
+
+        elif token[0] == 'pow':
+            result = (power(float(token[1]), float(token[2])))
+
+        elif token[0] == 'mod':
+            result = (mod(float(token[1]), float(token[2])))
+            
+        else:
+            print("Selection not valid")
+        print(result)
